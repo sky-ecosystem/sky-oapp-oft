a9bfce855bee344b10da2b020504768c47e8017a
Blockchain Warlock <39101443+TRileySchwarz@users.noreply.github.com>
2025-10-22 12:07:25 -0700

    Update pnpm lock (#32)
    * Update pnpm lock
    
    * Update contract licenses to apache 2.0

--- contracts/interfaces/IMintBurnVoidReturn.sol
+++ contracts/interfaces/IMintBurnVoidReturn.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.22;
 
 /// @title Interface for mintable and burnable tokens that DOES NOT return success boolean
